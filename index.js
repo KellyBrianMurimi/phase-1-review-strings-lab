0139e9d68aab651062a96c781dd1e1c53b5d0453
KellyBrianMurimi <kellybrianmurimi@gmail.com>
2023-09-20 11:20:55 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+const currentUser = 'Kelly Brian';
+const welcomeMessage = 'Welcome to Flatbook, ${Kelly Brian}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase()
+currentUser.slice(0, 1);
+const shortGreeting ='Welcome, ${K.slice(0, 1)}!';
